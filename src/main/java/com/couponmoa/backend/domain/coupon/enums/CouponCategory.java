fd97cc1c3343d067b4767721beb9876ab78340f6
ksmamks <minasithil33@gmail.com>
2025-04-02 11:35:03 +0900

    feat#3: Coupon Entity 추가

---
+++ src/main/java/com/couponmoa/backend/domain/coupon/enums/CouponCategory.java
@@ -0,0 +1,9 @@
+package com.couponmoa.backend.domain.coupon.enums;
+
+import lombok.Getter;
+import lombok.RequiredArgsConstructor;
+
+@Getter
+@RequiredArgsConstructor
+public enum CouponCategory {
+}
