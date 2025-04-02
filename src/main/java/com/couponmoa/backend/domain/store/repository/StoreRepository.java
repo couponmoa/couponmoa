03f2854251c53faa616d8c88551ca54e5816f7c1
myeongje <tiqmtiqm6541@gmail.com>
2025-04-02 19:20:06 +0900

    feat: #8  StoreRepository 로직 추가

--- src/main/java/com/couponmoa/backend/domain/store/repository/StoreRepository.java
+++ src/main/java/com/couponmoa/backend/domain/store/repository/StoreRepository.java
@@ -2,8 +2,12 @@ package com.couponmoa.backend.domain.store.repository;
 
 import com.couponmoa.backend.common.repository.BaseRepository;
 import com.couponmoa.backend.domain.store.entity.Store;
+import com.couponmoa.backend.domain.store.enums.StoreCategory;
 import org.springframework.stereotype.Repository;
+
+import java.util.List;
 
 @Repository
 public interface StoreRepository extends BaseRepository<Store, Long> {
+    List<Store> findByStoreCategory(StoreCategory storeCategory);
 }
