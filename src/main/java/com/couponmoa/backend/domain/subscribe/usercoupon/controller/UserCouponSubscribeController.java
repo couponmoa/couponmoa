0626eb93edd8f8d0db2e1f4a4ce3275cc5259e24
plusmate <psejin1478@gmail.com>
2025-04-02 17:03:07 +0900

    feat(#1):쿠폰 구독 리스트 Api 구현

--- src/main/java/com/couponmoa/backend/domain/subscribe/usercoupon/controller/UserCouponSubscribeController.java
+++ src/main/java/com/couponmoa/backend/domain/subscribe/usercoupon/controller/UserCouponSubscribeController.java
@@ -1,15 +1,20 @@
 package com.couponmoa.backend.domain.subscribe.usercoupon.controller;
 
 import com.couponmoa.backend.common.dto.ApiResponse;
+import com.couponmoa.backend.domain.subscribe.usercoupon.dto.response.FindSubscribeListResponse;
+import com.couponmoa.backend.domain.subscribe.usercoupon.entity.UserCouponSubscribe;
 import com.couponmoa.backend.domain.subscribe.usercoupon.service.UserCouponSubscribeService;
 import com.couponmoa.backend.domain.user.dto.AuthUser;
 import io.swagger.v3.oas.annotations.Operation;
 import io.swagger.v3.oas.annotations.Parameter;
 import io.swagger.v3.oas.annotations.tags.Tag;
 import lombok.RequiredArgsConstructor;
+import org.springframework.data.domain.Page;
 import org.springframework.http.ResponseEntity;
 import org.springframework.security.core.annotation.AuthenticationPrincipal;
 import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
 
 @Tag(name = "쿠폰 구독 API", description = "유저 쿠폰 구독 API")
 @RequiredArgsConstructor
@@ -37,5 +42,16 @@ public class UserCouponSubscribeController {
         userCouponSubServ.unSubscribeCoupon(user.getId(), couponId);
 
         return ResponseEntity.ok(ApiResponse.success(couponId + "번 쿠폰 구독 취소"));
+    }
+
+    @Operation(summary = "내 쿠폰 구독 목록 확인", description = "쿠폰 구독 목록 확인")
+    @GetMapping("/subscriptions")
+    public
+    ResponseEntity<ApiResponse<List<FindSubscribeListResponse>>> findCouponSubscribeList(@AuthenticationPrincipal AuthUser user,
+                                                               @RequestParam(defaultValue = "0") int page,
+                                                               @RequestParam(defaultValue = "10") int size) {
+        List<FindSubscribeListResponse> subscribeList = userCouponSubServ.findSubscribeList(user.getId(), page, size);
+
+        return ResponseEntity.ok(ApiResponse.success(subscribeList, "쿠폰 구독 목록 조회 성공"));
     }
 }
--- src/main/java/com/couponmoa/backend/domain/subscribe/usercoupon/repository/UserCouponSubscribeRepository.java
+++ src/main/java/com/couponmoa/backend/domain/subscribe/usercoupon/repository/UserCouponSubscribeRepository.java
@@ -4,9 +4,13 @@ import com.couponmoa.backend.common.repository.BaseRepository;
 import com.couponmoa.backend.domain.coupon.entity.Coupon;
 import com.couponmoa.backend.domain.subscribe.usercoupon.entity.UserCouponSubscribe;
 import com.couponmoa.backend.domain.user.entity.User;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 
 import java.util.Optional;
 
 public interface UserCouponSubscribeRepository extends BaseRepository<UserCouponSubscribe, Long> {
     Optional<UserCouponSubscribe> findByUserAndCoupon(User user, Coupon coupon);
+
+    Page<UserCouponSubscribe> findByUser(User user, Pageable pageable);
 }
