975168bf8a316c93c4cd94a1557b54e13ec7764a
나연 <kny5579@naver.com>
2025-04-03 13:44:08 +0900

    chore(#4): 탈퇴 로직에 Transactional 어노테이션 추가 및 requestDto에 생성자어노테이션 추가

--- src/main/java/com/couponmoa/backend/domain/user/dto/request/UserDeleteRequest.java
+++ src/main/java/com/couponmoa/backend/domain/user/dto/request/UserDeleteRequest.java
@@ -1,9 +1,11 @@
 package com.couponmoa.backend.domain.user.dto.request;
 
 import jakarta.validation.constraints.NotBlank;
+import lombok.AllArgsConstructor;
 import lombok.Getter;
 
 @Getter
+@AllArgsConstructor
 public class UserDeleteRequest {
     @NotBlank
     private String password;
