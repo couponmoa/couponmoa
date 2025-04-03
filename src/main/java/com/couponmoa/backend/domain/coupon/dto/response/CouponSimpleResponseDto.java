bb49892bf68f6627f63e7e386c79c7331d4e3fc4
ksmamks <minasithil33@gmail.com>
2025-04-03 15:53:57 +0900

    feat(#03): Coupon Api findOne,findAll

--- src/main/java/com/couponmoa/backend/domain/coupon/dto/response/CouponSimpleResponseDto.java
+++ src/main/java/com/couponmoa/backend/domain/coupon/dto/response/CouponSimpleResponseDto.java
@@ -1,4 +1,32 @@
 package com.couponmoa.backend.domain.coupon.dto.response;
 
+import com.couponmoa.backend.domain.coupon.entity.Coupon;
+import lombok.Builder;
+import lombok.Getter;
+
+import java.math.BigDecimal;
+import java.time.LocalDateTime;
+
+@Getter
+@Builder
 public class CouponSimpleResponseDto {
+    private Long id;
+    private String name;
+    private int availableQuantity;
+    private BigDecimal discountAmount;
+    private BigDecimal discountRate;
+    private LocalDateTime startDate;
+    private LocalDateTime endDate;
+
+    public static CouponSimpleResponseDto toDto(Coupon coupon) {
+        return CouponSimpleResponseDto.builder()
+                .id(coupon.getId())
+                .name(coupon.getName())
+                .availableQuantity(coupon.getAvailableQuantity())
+                .discountAmount(coupon.getDiscountAmount())
+                .discountRate(coupon.getDiscountRate())
+                .startDate(coupon.getStartDate())
+                .endDate(coupon.getEndDate())
+                .build();
+    }
 }
--- src/main/java/com/couponmoa/backend/domain/coupon/repository/CouponRepository.java
+++ src/main/java/com/couponmoa/backend/domain/coupon/repository/CouponRepository.java
@@ -3,10 +3,16 @@ package com.couponmoa.backend.domain.coupon.repository;
 import com.couponmoa.backend.common.exception.ErrorCode;
 import com.couponmoa.backend.common.repository.BaseRepository;
 import com.couponmoa.backend.domain.coupon.entity.Coupon;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
+import org.springframework.data.jpa.repository.Query;
 
 public interface CouponRepository extends BaseRepository<Coupon,Long> {
     @Override
     default Coupon findByIdOrElseThrow(Long aLong, ErrorCode errorCode) {
         return BaseRepository.super.findByIdOrElseThrow(aLong, errorCode);
     }
+
+    @Query("SELECT c FROM Coupon c ORDER BY (c.totalQuantity - c.availableQuantity) DESC")
+    Page<Coupon> findAllSortedByIssuedQuantity(Pageable pageable);
 }
