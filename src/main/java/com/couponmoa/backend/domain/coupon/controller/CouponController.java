98fae198e1f86fccfbb7a8aa7c4a68b741ed6719
ksmamks <minasithil33@gmail.com>
2025-04-03 15:53:57 +0900

    feat(#03): Coupon Api findOne,findAll

--- src/main/java/com/couponmoa/backend/domain/coupon/controller/CouponController.java
+++ src/main/java/com/couponmoa/backend/domain/coupon/controller/CouponController.java
@@ -3,15 +3,21 @@ package com.couponmoa.backend.domain.coupon.controller;
 import com.couponmoa.backend.common.dto.ApiResponse;
 import com.couponmoa.backend.domain.coupon.dto.request.CouponSaveRequestDto;
 import com.couponmoa.backend.domain.coupon.dto.request.CouponUpdateRequestDto;
+import com.couponmoa.backend.domain.coupon.dto.response.CouponDetailResponseDto;
 import com.couponmoa.backend.domain.coupon.dto.response.CouponResponseDto;
+import com.couponmoa.backend.domain.coupon.dto.response.CouponSimpleResponseDto;
+import com.couponmoa.backend.domain.coupon.service.CouponReadService;
 import com.couponmoa.backend.domain.coupon.service.CouponService;
+import com.couponmoa.backend.domain.user.dto.AuthUser;
 import com.couponmoa.backend.domain.user.enums.UserRole;
 import io.swagger.v3.oas.annotations.Operation;
 import io.swagger.v3.oas.annotations.tags.Tag;
 import jakarta.validation.Valid;
 import lombok.RequiredArgsConstructor;
+import org.springframework.data.domain.Page;
 import org.springframework.http.ResponseEntity;
 import org.springframework.security.access.annotation.Secured;
+import org.springframework.security.core.annotation.AuthenticationPrincipal;
 import org.springframework.web.bind.annotation.*;
 
 // 차후에 responsebody 메세지 추가예정.
@@ -22,6 +28,7 @@ import org.springframework.web.bind.annotation.*;
 public class CouponController {
 
     private final CouponService couponService;
+    private final CouponReadService couponReadService;
 
     @Operation(summary = "쿠폰 생성", description = "관리자가 새로운 쿠폰을 생성함.")
     @Secured(UserRole.Authority.ADMIN)
@@ -29,6 +36,25 @@ public class CouponController {
     public ResponseEntity<ApiResponse<CouponResponseDto>> createCoupon(
             @Valid @RequestBody CouponSaveRequestDto requestDto) {
         return ResponseEntity.ok(couponService.createCoupon(requestDto));
+    }
+
+    @Operation(summary = "쿠폰 목록 조회", description = "쿠폰 목록을 페이징하여 조회함.") //현재 정렬 순서는 issuedQuantity, 이후에 검색어 or 정렬 기준 추가, 모든 데이터의 issuedQ가 0일때 조회 기준도 있어야함.
+    @GetMapping
+    public ResponseEntity<ApiResponse<Page<CouponSimpleResponseDto>>> findAllCoupons(
+            @RequestParam(defaultValue = "0") int page,
+            @RequestParam(defaultValue = "10") int size) {
+
+        ApiResponse<Page<CouponSimpleResponseDto>> response = couponReadService.findAllCoupons(page, size);
+        return ResponseEntity.ok(response);
+    }
+
+    @Operation(summary = "쿠폰 상세 조회", description = "특정 쿠폰의 상세 정보를 조회함.")
+    @GetMapping("/{couponId}")
+    public ResponseEntity<ApiResponse<CouponDetailResponseDto>> findCoupon(
+            @PathVariable Long couponId,
+            @AuthenticationPrincipal AuthUser authUser) {
+        ApiResponse<CouponDetailResponseDto> response = couponReadService.findCoupon(couponId, authUser);
+        return ResponseEntity.ok(response);
     }
 
     @Operation(summary = "쿠폰 수정", description = "관리자가 특정 쿠폰 정보를 수정함.")
--- src/main/java/com/couponmoa/backend/domain/coupon/service/CouponReadService.java
+++ src/main/java/com/couponmoa/backend/domain/coupon/service/CouponReadService.java
@@ -1,13 +1,19 @@
 package com.couponmoa.backend.domain.coupon.service;
 
 import com.couponmoa.backend.common.dto.ApiResponse;
+import com.couponmoa.backend.common.exception.ApplicationException;
+import com.couponmoa.backend.common.exception.ErrorCode;
 import com.couponmoa.backend.domain.coupon.dto.response.CouponDetailResponseDto;
 import com.couponmoa.backend.domain.coupon.dto.response.CouponSimpleResponseDto;
+import com.couponmoa.backend.domain.coupon.entity.Coupon;
 import com.couponmoa.backend.domain.coupon.repository.CouponRepository;
 import com.couponmoa.backend.domain.user.dto.AuthUser;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.data.domain.Page;
+import org.springframework.data.domain.PageRequest;
+import org.springframework.data.domain.Pageable;
+import org.springframework.data.domain.Sort;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
@@ -19,12 +25,25 @@ public class CouponReadService {
 
     private final CouponRepository couponRepository;
 
-    public ApiResponse<Page<CouponSimpleResponseDto>> findCoupons() {
-        return null;
+    public ApiResponse<Page<CouponSimpleResponseDto>> findAllCoupons(int page, int size) {
+        Pageable pageable = PageRequest.of(page-1, size);
+
+        Page<Coupon> coupons = couponRepository.findAllSortedByIssuedQuantity(pageable);
+
+        //테스트 로깅
+        log.info("조회된 쿠폰 개수: {}",coupons.getTotalElements());
+        for (Coupon coupon : coupons) {
+            log.info("쿠폰 id: {}, 쿠폰 이름: {}", coupon.getId(), coupon.getName());
+        }
+
+        return ApiResponse.success(coupons.map(CouponSimpleResponseDto::toDto));
     }
 
     public ApiResponse<CouponDetailResponseDto> findCoupon(Long couponId, AuthUser authUser) {
 
-        return null;
+        Coupon coupon = couponRepository.findById(couponId)
+                .orElseThrow(() -> new ApplicationException(ErrorCode.COUPON_NOT_FOUND));
+
+        return ApiResponse.success(CouponDetailResponseDto.toDto(coupon));
     }
 }
