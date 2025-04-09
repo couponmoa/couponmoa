039b0deeb9de34fcb2f5a475f81c9642b36179ed
나연 <kny5579@naver.com>
2025-04-09 13:26:00 +0900

    refactor(#70): 만료 알림 엔티티 수정. 알림 타입 enum 추가

--- src/main/java/com/couponmoa/backend/notification/entity/Notification.java
+++ src/main/java/com/couponmoa/backend/notification/entity/Notification.java
@@ -2,6 +2,7 @@ package com.couponmoa.backend.notification.entity;
 
 import com.couponmoa.backend.common.entity.BaseEntity;
 import com.couponmoa.backend.domain.usercoupon.entity.UserCoupon;
+import com.couponmoa.backend.notification.enums.NotificationType;
 import jakarta.persistence.*;
 import jakarta.validation.constraints.NotNull;
 import lombok.AccessLevel;
@@ -29,4 +30,14 @@ public class Notification extends BaseEntity {
     @JoinColumn(name = "user_coupon_id", nullable = false)
     @OnDelete(action = OnDeleteAction.CASCADE)
     private UserCoupon userCoupon;
+
+    @Enumerated(EnumType.STRING)
+    private NotificationType type;
+
+    public Notification(String contents, boolean isNotified, UserCoupon userCoupon, NotificationType type) {
+        this.contents = contents;
+        this.isNotified = isNotified;
+        this.userCoupon = userCoupon;
+        this.type = type;
+    }
 }
---
+++ src/main/java/com/couponmoa/backend/notification/enums/NotificationType.java
@@ -0,0 +1,5 @@
+package com.couponmoa.backend.notification.enums;
+
+public enum NotificationType {
+    EXPIRE_SOON, COUPON_SUBSCRIBE, STORE_SUBSCRIBE
+}
