bdcf7baec429728f2925b3c514ec8c48ee6fc9b4
ksmamks <minasithil33@gmail.com>
2025-04-09 20:21:28 +0900

    test : add coupon Serivce&Controller tests

--- src/test/java/com/couponmoa/backend/domain/coupon/service/CouponReadServiceTest.java
+++ src/test/java/com/couponmoa/backend/domain/coupon/service/CouponReadServiceTest.java
@@ -2,6 +2,7 @@ package com.couponmoa.backend.domain.coupon.service;
 
 import com.couponmoa.backend.common.dto.ApiResponse;
 import com.couponmoa.backend.common.exception.ApplicationException;
+import com.couponmoa.backend.domain.coupon.dto.response.CouponDetailResponseDto;
 import com.couponmoa.backend.domain.coupon.dto.response.CouponSimpleResponseDto;
 import com.couponmoa.backend.domain.coupon.entity.Coupon;
 import com.couponmoa.backend.domain.coupon.repository.CouponRepository;
@@ -14,6 +15,7 @@ import org.mockito.MockitoAnnotations;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.PageImpl;
 import org.springframework.data.domain.Pageable;
+import org.springframework.test.util.ReflectionTestUtils;
 
 import java.util.List;
 import java.util.Optional;
@@ -36,7 +38,7 @@ class CouponReadServiceTest {
     }
 
     @Test
-    void 쿠폰_목록_조회_성공() {
+    void 쿠폰_목록_조회_0건_정상작동() {
         // given
         List<Coupon> couponList = List.of(mock(Coupon.class));
         Page<Coupon> page = new PageImpl<>(couponList);
@@ -50,6 +52,39 @@ class CouponReadServiceTest {
         // then
         assertThat(result.getData().getTotalElements()).isEqualTo(1);
         verify(couponRepository).findAllSortedByIQ(any(Pageable.class));
+    }
+
+    @Test
+    void 쿠폰_목록_조회_없을시_예외처리() {
+        // given
+        Page<Coupon> page = new PageImpl<>(List.of());
+        when(couponRepository.findAllSortedByIQ(any(Pageable.class))).thenReturn(page);
+
+        // when
+        ApiResponse<Page<CouponSimpleResponseDto>> result = couponReadService.findAllCoupons(1, 10);
+
+        // then
+        assertThat(result.getData().getContent()).isEmpty();
+        assertThat(result.getData().getTotalElements()).isEqualTo(0);
+    }
+
+    @Test
+    void 쿠폰_상세_조회_성공() {
+        // given
+        Coupon coupon = new Coupon();
+        ReflectionTestUtils.setField(coupon, "id", 1L);
+        ReflectionTestUtils.setField(coupon, "name", "테스트용_쿠폰");
+
+        when(couponRepository.findById(1L)).thenReturn(Optional.of(coupon));
+
+        // when
+        ApiResponse<CouponDetailResponseDto> result = couponReadService.findCoupon(1L, mock(AuthUser.class));
+
+        // then
+        assertThat(result.getData().getId()).isEqualTo(1L);
+        assertThat(result.getData().getName()).isEqualTo("테스트용_쿠폰");
+
+        verify(couponRepository).findById(1L);
     }
 
     @Test
