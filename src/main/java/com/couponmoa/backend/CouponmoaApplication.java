c0dc183294cb72d6759349cad55a00b449589722
iboyeon0816 <iboyeon0816@gmail.com>
2025-04-11 19:41:00 +0900

    feat(#69): 비동기 메서드 예외 발생 시 로그 출력 추가

--- src/main/java/com/couponmoa/backend/CouponmoaApplication.java
+++ src/main/java/com/couponmoa/backend/CouponmoaApplication.java
@@ -3,9 +3,7 @@ package com.couponmoa.backend;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.data.web.config.EnableSpringDataWebSupport;
-import org.springframework.scheduling.annotation.EnableAsync;
 
-@EnableAsync
 @SpringBootApplication
 @EnableSpringDataWebSupport(pageSerializationMode = EnableSpringDataWebSupport.PageSerializationMode.VIA_DTO)
 public class CouponmoaApplication {
