7ef85e67cb3bbf2b6c9acf3823328caf3e290d56
ksmamks <minasithil33@gmail.com>
2025-04-14 21:28:42 +0900

    fix(#82): fix simpleDto compile error and add findCouponV2 feature

--- src/main/java/com/couponmoa/backend/domain/coupon/service/CouponReadServiceV2.java
+++ src/main/java/com/couponmoa/backend/domain/coupon/service/CouponReadServiceV2.java
@@ -1,10 +1,16 @@
 package com.couponmoa.backend.domain.coupon.service;
 
+import com.couponmoa.backend.common.exception.ApplicationException;
+import com.couponmoa.backend.common.exception.ErrorCode;
 import com.couponmoa.backend.domain.coupon.dto.request.CouponCursor;
 import com.couponmoa.backend.domain.coupon.dto.request.CouponSearchByStoreRequestDto;
+import com.couponmoa.backend.domain.coupon.dto.response.CouponDetailResponseDto;
 import com.couponmoa.backend.domain.coupon.dto.response.CouponSimpleResponseDto;
+import com.couponmoa.backend.domain.coupon.entity.Coupon;
 import com.couponmoa.backend.domain.coupon.enums.CouponStatus;
 import com.couponmoa.backend.domain.coupon.repository.CouponQueryDslRepository;
+import com.couponmoa.backend.domain.coupon.repository.CouponRepository;
+import com.couponmoa.backend.domain.user.dto.AuthUser;
 import lombok.RequiredArgsConstructor;
 import org.springframework.cache.annotation.Cacheable;
 import org.springframework.data.domain.Page;
@@ -19,10 +25,11 @@ import java.util.List;
 public class CouponReadServiceV2 {
 
     private final CouponQueryDslRepository couponQueryDslRepository;
+    private final CouponRepository couponRepository;
 
     @Cacheable(
             value = "coupons",
-            key = "T(com.couponmoa.backend.domain.coupon.util.CacheKeyGenerator).generateCacheKey(#status, #cursor, #size, #page)"
+            key = "T(com.couponmoa.backend.domain.coupon.util.CacheKeyGenerator).generateCacheKey(#status, #cursor, #size)"
     )
     public List<CouponSimpleResponseDto> findCouponsByKeyword(CouponStatus status, CouponCursor cursor, int size) {
         return couponQueryDslRepository.searchCouponsByKeyword(status, cursor, size);
@@ -48,5 +55,15 @@ public class CouponReadServiceV2 {
                 requestDto.getStartDate(),
                 pageable
         );
+    }
+
+    @Cacheable(
+            value = "couponDetails",
+            key = "T(com.couponmoa.backend.domain.coupon.util.CacheKeyGenerator).generateCacheKey(#couponId)"
+    )
+    public CouponDetailResponseDto findCoupon(Long couponId, AuthUser authUser) {
+        Coupon coupon = couponRepository.findById(couponId)
+                .orElseThrow(() -> new ApplicationException(ErrorCode.COUPON_NOT_FOUND));
+        return CouponDetailResponseDto.toDto(coupon);
     }
 }
