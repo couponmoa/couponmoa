bcf5fd015c09c98bd034f7022866b238d1f8d691
ksmamks <minasithil33@gmail.com>
2025-04-17 18:59:15 +0900

    test(#112): init commit

---
+++ src/test/java/com/couponmoa/backend/domain/coupon/service/CouponServiceV2Test.java
@@ -0,0 +1,7 @@
+package com.couponmoa.backend.domain.coupon.service;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class CouponServiceV2Test {
+
+}
---
+++ src/test/java/com/couponmoa/backend/domain/store/service/StoreServiceV2Test.java
@@ -0,0 +1,7 @@
+package com.couponmoa.backend.domain.store.service;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class StoreServiceV2Test {
+
+}
